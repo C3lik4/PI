35e34718d9d9ee1a16365e9972dc1767e6be753b
C3lik4 <80261681+C3lik4@users.noreply.github.com>
2021-05-17 11:32:30 +0200

    nuevos problemas en la funcionalidad :(

--- src/Entity/Clases/ventas.php
+++ src/Entity/Clases/ventas.php
@@ -26,12 +26,29 @@ class ventas extends Conexion implements crudInterface{
             $ano = $_POST["ano"];
             $km = $_POST["km"];
             $precio = $_POST["precio"];
+
             $fotos = $_FILES["fotos"]['name'];
             $directorio_fotos = $_FILES["fotos"]['tmp_name'];
 
-       
+        }
 
-            move_uploaded_file($directorio_fotos , __DIR__.'/../../../public/anuncios/'. $fotos);
+                    
+            if(isset($_POST['submit'])){
+            // Count total files
+            $countfiles = count($_FILES['fotos']['name']);
+            
+            // Looping all files
+            for($i=0;$i<$countfiles;$i++){
+            $fotos = $_FILES['file']['name'][$i];
+            
+            // Upload file
+            move_uploaded_file($_FILES['fotos']['tmp_name'][$i],__DIR__.'/../../../public/anuncios/'. $fotos);
+                
+            }
+        } 
+            
+
+            //move_uploaded_file($directorio_fotos , __DIR__.'/../../../public/anuncios/'. $fotos);
             
 
         
