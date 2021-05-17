e1c9157d814d7b4d4d80c2d4266527cf778f3232
C3lik4 <80261681+C3lik4@users.noreply.github.com>
2021-05-17 10:22:18 +0200

    avances en la funcionaliadad de venta , problema al seleccionar varios archivos

--- src/Entity/Clases/ventas.php
+++ src/Entity/Clases/ventas.php
@@ -15,21 +15,24 @@ class ventas extends Conexion implements crudInterface{
 
     }
 
+    //agafar timestamp y numero aleatori per a fer identificador de foto de coche
     public function add($values = null){
         if (count($_POST) > 0) {
         
 
-        $marca = $_POST["marca"];
-        $modelo = $_POST["modelo"];
-        $color = $_POST["color"];
-        $ano = $_POST["ano"];
-        $km = $_POST["km"];
-        $precio = $_POST["precio"];
-        $fotos = $_FILES["fotos"]['name'];
-        $directorio_fotos = $_FILES["fotos"]['tmp_name'];
+            $marca = $_POST["marca"];
+            $modelo = $_POST["modelo"];
+            $color = $_POST["color"];
+            $ano = $_POST["ano"];
+            $km = $_POST["km"];
+            $precio = $_POST["precio"];
+            $fotos = $_FILES["fotos"]['name'];
+            $directorio_fotos = $_FILES["fotos"]['tmp_name'];
 
+       
 
-        move_uploaded_file($directorio_fotos , __DIR__.'/../../../public/anuncios/'. $fotos);
+            move_uploaded_file($directorio_fotos , __DIR__.'/../../../public/anuncios/'. $fotos);
+            
 
         
 
