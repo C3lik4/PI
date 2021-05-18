0e4bb18efd6a1ddaaff6f0da438669ff7a1276d0
C3lik4 <80261681+C3lik4@users.noreply.github.com>
2021-05-18 10:22:42 +0200

    mas avances

--- public/js/autogenerar_anuncios.js
+++ public/js/autogenerar_anuncios.js
@@ -23,71 +23,82 @@ function main(){
 
     function rellenador_anuncios(array_anuncios_json){
         
+        let contenedor_anuncios = document.getElementById("contenedor");
 
-        for (x in array_anuncios_json) {
+        for(let z = 0; z < array_anuncios_json.length; z = z+3){
+    
 
-           let contenedor_anuncios = document.getElementById("contenedor");
-            
+           
+            console.log("retard");
            let fila_tarjeta = document.createElement("div");
            fila_tarjeta.className = "row";
            contenedor_anuncios.appendChild(fila_tarjeta);
 
-           let primer_div = document.createElement("div");
-           primer_div.className="col-lg-4 col-md-6 mb-4";
-           fila_tarjeta.appendChild(primer_div);
-
-           let segundo_div = document.createElement("div");
-           segundo_div.className = "card h-100 lista bordes";
-           primer_div.appendChild(segundo_div);
-
-           let imagen = document.createElement("img");
-           imagen.className = "card-img-top";
-           segundo_div.appendChild(imagen);
-           //imagen.setAttribute("src","anuncios/array_anuncios_json[x].foto")
-
-           let div_cuerpo = document.createElement("div");
-           div_cuerpo.className = "card-body";
-           segundo_div.appendChild(div_cuerpo);
-
-           let marca = document.createElement("p");
-           marca.className = "items";
-           marca.appendChild(div_cuerpo);
-           marca.value="";
-
-           let precio = document.createElement("h5");
-           precio.appendChild(div_cuerpo);
-           precio.value = array_anuncios_json[x].precio;
+           for (x in array_anuncios_json){
+               console.log("gilipollas");
+            let primer_div = document.createElement("div");
+            primer_div.className="col-lg-4 col-md-6 mb-4";
+            fila_tarjeta.appendChild(primer_div);
+ 
+            let segundo_div = document.createElement("div");
+            segundo_div.className = "card h-100 lista bordes";
+            primer_div.appendChild(segundo_div);
+ 
+            let imagen = document.createElement("img");
+            imagen.className = "card-img-top";
+            //imagen.setAttribute("src","anuncios/array_anuncios_json[x].foto");
+            segundo_div.appendChild(imagen);
+            
+ 
+            let div_cuerpo = document.createElement("div");
+            div_cuerpo.className = "card-body";
+            segundo_div.appendChild(div_cuerpo);
+ 
+            let marca = document.createElement("p");
+            marca.className = "items";
+            div_cuerpo.appendChild(marca);
+            
+ 
+            let precio = document.createElement("h5");
+            precio.innerHTML = array_anuncios_json[x].precio;
+            div_cuerpo.appendChild(precio);
+            
+            
+            let lista = document.createElement("ul");
+            lista.className = "card-text descripcion_coche";
+            div_cuerpo.appendChild(lista);
+ 
+            let modelo = document.createElement("li");
+            lista.appendChild(modelo);
+            
+ 
+            let color = document.createElement("li");
+            color.innerHTML = array_anuncios_json[x].color;
+            lista.appendChild(color);
+            
+ 
+            let año = document.createElement("li");
+            año.innerHTML = array_anuncios_json[x].ano;
+            lista.appendChild(año);
+            
+ 
+            let km = document.createElement("li");
+            km.innerHTML = array_anuncios_json[x].km;
+            lista.appendChild(km);
+            
+ 
+            let footer = document.createElement("div");
+            footer.className = "card-footer center fuente_navbar";
+             segundo_div.appendChild(footer);
+ 
+             let boton = document.createElement("button");
+             boton.className = "btn btn--radius-2 btn--red";
+             boton.setAttribute("data-toggle","modal")
+             boton.setAttribute("data-target","#exampleModalCenter")
+ 
+             footer.appendChild(boton);
+           }
            
-           let lista = document.createElement("ul");
-           lista.className = "card-text descripcion_coche";
-           lista.appendChild(div_cuerpo);
-
-           let modelo = document.createElement("li");
-           modelo.appendChild(lista);
-           modelo.value="";
-
-           let color = document.createElement("li");
-           color.appendChild(lista);
-           color.value = array_anuncios_json[x].color;
-
-           let año = document.createElement("li");
-           año.appendChild(lista);
-           año.value = array_anuncios_json[x].ano;
-
-           let km = document.createElement("li");
-           km.appendChild(lista);
-           km.value = array_anuncios_json[x].km;
-
-           let footer = document.createElement("div");
-           footer.className = "card-footer center fuente_navbar";
-            segundo_div.appendChild(footer);
-
-            let boton = document.createElement("button");
-            boton.className = "btn btn--radius-2 btn--red";
-            boton.setAttribute("data-toggle","modal")
-            boton.setAttribute("data-target","#exampleModalCenter")
-
-            footer.appendChild(boton);
         }
 
     }
