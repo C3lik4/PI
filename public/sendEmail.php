184e9c1f6f9a9e46587d1566a4ca224d000c00dd
Joorgi <>
2021-05-21 09:58:38 +0200

    función enviar Email modificada

--- public/sendEmail.php
+++ public/sendEmail.php
@@ -12,33 +12,30 @@ require '../src/SMTP.php';
 //Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
-try {
-    $contactName = $_POST["contactName"];
-    $contactMail = $_POST["contactEmail"];
-    $contactMessage = $_POST["contactMessage"];
 
-    //Server settings
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = "smtp.gmail.com";                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = "legendarymotorsport1825@gmail.com";                     //SMTP username
-    $mail->Password   = 'tinckam1319';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+$contactName = $_POST["contactName"];
+$contactMail = $_POST["contactEmail"];
+$contactMessage = $_POST["contactMessage"];
+//Server settings
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+$mail->isSMTP();                                            //Send using SMTP
+$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+$mail->Username   = 'legendarymotorsport1825@gmail.com';                     //SMTP username
+$mail->Password   = 'tinckam1319';                               //SMTP password
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+$mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-    //Recipients
-    $mail->setFrom("legendarymotorsport1825@gmail.com", 'Mailer');
-    $mail->addAddress("legendarymotorsport1825@gmail.com", 'Joe User');     //Add a recipient
+//Recipients
+$mail->setFrom('legendarymotorsport1825@gmail.com', 'Admin');
+$mail->addAddress('legendarymotorsport1825@gmail.com', 'Joe User');     //Add a recipient
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = $contactMail;
-    $mail->Body    = "Hola mi nombre es " . $contactName . " les contacto con el siguiente motivo: $contactMessage";
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    $mail->send();
-    header("Location:indexSesion.php");
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+//Content
+$mail->isHTML(true);                                  //Set email format to HTML
+$mail->Subject = $contactMail;
+$mail->Body    = "Hola mi nombre es " . $contactName . " les contacto con el siguiente motivo: $contactMessage";
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+$mail->send();
+header("Location:indexSesion.php");
