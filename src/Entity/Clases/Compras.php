b7a1d3203fb89d808abb760653b903c238360aac
Joorgi <>
2021-05-22 11:29:24 +0200

    Añadida la clase Compras.php

---
+++ src/Entity/Clases/Compras.php
@@ -0,0 +1,20 @@
+<?php
+
+class Compras extends Conexion implements CrudInterface
+{
+    public function get()
+    {
+    }
+
+    public function add($values)
+    {
+    }
+
+    public function update($values)
+    {
+    }
+
+    public function delete($id)
+    {
+    }
+}
